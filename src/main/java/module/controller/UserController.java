bd81f21cbdcf98b4d9c3eaf25e1b73f6b43c4434
tianyi <793187547@qq.com>
2017-12-22 17:33:33 +0800

    升级spring4

--- src/main/java/module/controller/UserController.java
+++ src/main/java/module/controller/UserController.java
@@ -4,15 +4,12 @@ import module.model.User;
 import module.service.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -33,7 +30,7 @@ public class UserController {
 
     @RequestMapping(value = "/getUserById", method = RequestMethod.GET)
     public @ResponseBody
-    User getUserById(@RequestParam(value="id",defaultValue="1") Long id) {
+    User getUserById(@RequestParam(value = "id", defaultValue = "1") Long id) {
         log.info("查询所有用户信息");
         return userService.getUserById(id);
     }
